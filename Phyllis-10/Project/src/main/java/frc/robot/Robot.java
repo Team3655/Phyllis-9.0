6318bd319854930a80d52824412d751e94f3f307
Nageek15 <kbentley15@gmail.com>
2019-02-11 21:10:54 -0500

    One Joystsick Arcade Drive

--- Phyllis-10/Project/src/main/java/frc/robot/Robot.java
+++ Phyllis-10/Project/src/main/java/frc/robot/Robot.java
@@ -7,36 +7,47 @@
 
 package frc.robot;
 
-import com.revrobotics.CANSparkMaxLowLevel;
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import com.revrobotics.*;
+
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
+  private DifferentialDrive robot;
+  private Joystick leftStick;
+  //private Joystick m_rightStick;
   private CANSparkMax leftFront=new CANSparkMax(10,CANSparkMaxLowLevel.MotorType.kBrushless);
   private CANSparkMax leftBack=new CANSparkMax(11,CANSparkMaxLowLevel.MotorType.kBrushless);
   private CANSparkMax rightFront=new CANSparkMax(13,CANSparkMaxLowLevel.MotorType.kBrushless);
   private CANSparkMax rightBack=new CANSparkMax(12,CANSparkMaxLowLevel.MotorType.kBrushless);
   @Override
   public void robotInit() {
+    
     leftBack.follow(leftFront);
     rightBack.follow(rightFront);
-    m_myRobot = new DifferentialDrive(leftFront,rightFront);
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    robot = new DifferentialDrive(leftFront,rightFront);
+    leftStick = new Joystick(0);
+    
+    //Button button1 = new JoystickButton(leftStick, 1);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.arcadeDrive(m_leftStick.getY(), m_rightStick.getX());
+    //driving one joystick
+    robot.arcadeDrive(leftStick.getY()*-1, leftStick.getX(),true);
+    
+  }
+  /**
+   * Prints debug information to console
+   */
+  private void debug(){
+    System.out.println("X:"+leftStick.getX());
+    System.out.println("Y: "+leftStick.getY());
+    System.out.println("Left: "+leftFront.getAppliedOutput());
+    System.out.println("Right: "+rightFront.getAppliedOutput());
   }
 }
